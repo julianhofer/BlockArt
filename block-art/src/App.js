cb986f8086db39a50b2dd468cb4c0ea22fd89edc
unknown <jh187@hdm-stuttgart.de>
2020-01-08 21:06:42 +0100

    working web3 connection with metamost

--- block-art/src/App.js
+++ block-art/src/App.js
@@ -1,19 +1,70 @@
 import React, { Component } from 'react';
 import './App.css';
+import './connectBlockchain';
 import getConnection from './connectBlockchain';
 
 
 class App extends Component {
 
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     account: null,
+  //     web3: null,
+  //   }
+  // }
 
-  UNSAFE_componentWillMount() {
-    getConnection();
-  }
+  state = {
+    web3: null,
+    account: null
+  };
+
+
+
+
+  componentDidMount = async () => {
+
+
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getConnection();
+
+
+      // Use web3 to get the user's accounts.
+      const account = await web3.eth.getAccounts();
+
+      this.setState({ account: account[0] });
+      console.log("Account: ", account[0])
+
+      const network = await web3.eth.net.getNetworkType();
+      console.log("Netzwerk: ", network);
+
+      // Set web3, accounts, and contract to the state, and then proceed with an
+      // example of interacting with the contract's methods.
+      this.setState({
+        web3,
+        account
+      });
+
+    } catch (error) {
+
+      alert(
+        `Failed to connect`,
+      );
+      console.error(error);
+    }
+  };
+
+  // UNSAFE_componentWillMount() {
+  //   this.getConnection();
+  // }
+
 
   render() {
     return (
       <div className="container">
         <h1>BlockArt</h1>
+        <p>Accountadresse: {this.state.account}</p>
       </div>
     );
   }
--- block-art/src/connectBlockchain.js
+++ block-art/src/connectBlockchain.js
@@ -1,35 +1,97 @@
 import Web3 from "web3";
 
-const getConnection = () => {
-
-    const provider = new Web3(Web3.givenProvider || "http://localhost:7545");
-
-
-    const web3 = new Web3(provider);
-    console.log("Es wird die lokale Blockchain auf Port 7545 verwendet.");
-
-
-    const network = web3.eth.net.getNetworkType();
-    console.log("Netzwerk: " + network);
-
-};
+const getConnection = () =>
+    new Promise((resolve, reject) => {
+        // Wait for loading completion to avoid race conditions with web3 injection timing.
+        window.addEventListener("load", async () => {
+            // Modern dapp browsers...
+            if (window.ethereum) {
+                const web3 = new Web3(window.ethereum);
+                try {
+                    // Request account access if needed
+                    await window.ethereum.enable();
+                    // Acccounts now exposed
+                    resolve(web3);
+                } catch (error) {
+                    reject(error);
+                }
+            }
+            // Legacy dapp browsers...
+            else if (window.web3) {
+                // Use Mist/MetaMask's provider.
+                const web3 = window.web3;
+                console.log("Injected web3 detected.");
+                resolve(web3);
+            }
+            // Fallback to localhost; use dev console port by default...
+            else {
+                const provider = new Web3.providers.HttpProvider(
+                    "http://127.0.0.1:7545"
+                );
+                const web3 = new Web3(provider);
+                console.log("Es wird die lokale Blockchain auf Port 7545 verwendet.");
+                resolve(web3);
+            }
+        });
+    });
 
 export default getConnection;
 
-
-// const getConnection = () =>
+// const getConnection = () => {
 //     new Promise((resolve, reject) => {
+//         window.addEventListener("load", async () => {
 
-//         const provider = new Web3.givenProvider.HttpProvider(
-//             "http://127.0.0.1:7545"
-//         );
-//         const web3 = new Web3(Provider);
-//         console.log("Es wir die lokale Blockchain auf Port 7545 verwendet.");
-//         resolve(web3);
+//             if (window.ethereum) {
+//                 const web3 = new Web3(window.ethereum);
+//                 try{
 
-//         const network = await web3.eth.net.getNetworkType();
-//         console.log("Netzwerk: " + network);
 
+//                 const web3 = window.web3;
+//                 console.log("Injected web3 detected.");
+//                 resolve(web3);
+//             } else {
+
+
+//                 const web3 = new Web3(window.ethereum);
+//                 // const provider = new Web3(Web3.providers.HttpProvider || "http://127.0.0.1:7545");
+//                 // const web3 = new Web3(provider);
+//                 // console.log("Es wird die lokale Blockchain auf Port 7545 verwendet.");
+//                 const provider = new Web3.providers.HttpProvider(
+//                     "http://127.0.0.1:7545"
+//                 );
+//                 const web3 = new Web3(provider);
+//                 console.log("Es wird die lokale Blockchain auf Port 7545 verwendet.");
+//                 resolve(web3);
+
+//                 const network = await web3.eth.net.getNetworkType();
+//                 console.log("Netzwerk: ", network);
+//                 const account = await web3.eth.getAccounts();
+//                 console.log("Account: ", account[1]);
+//             }
+//         }
+//         });
 //     });
+// }
+
+
+
 
 // export default getConnection;
+
+
+// // const getConnection = () =>
+// //     new Promise((resolve, reject) => {
+
+// //         const provider = new Web3.givenProvider.HttpProvider(
+// //             "http://127.0.0.1:7545"
+// //         );
+// //         const web3 = new Web3(Provider);
+// //         console.log("Es wir die lokale Blockchain auf Port 7545 verwendet.");
+// //         resolve(web3);
+
+// //         const network = await web3.eth.net.getNetworkType();
+// //         console.log("Netzwerk: " + network);
+
+// //     });
+
+// // export default getConnection;
