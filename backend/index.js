275e402a086614d2349043b1f194d68047e3f34a
unknown <jh187@hdm-stuttgart.de>
2020-01-14 15:33:34 +0100

    add CRUD functions

--- backend/index.js
+++ backend/index.js
@@ -15,3 +15,58 @@ conn.connect((err) => {
     if (err) throw err;
     console.log('Mysql Connected...');
 });
+
+//show all users
+app.get('/api/users', (req, res) => {
+    let sql = "SELECT * FROM users";
+    let query = conn.query(sql, (err, results) => {
+        if (err) throw err;
+        res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
+    });
+});
+
+//show single user by id
+app.get('/api/users/:id', (req, res) => {
+    let sql = "SELECT * FROM users WHERE user_id=" + req.params.user_id;
+    let query = conn.query(sql, (err, results) => {
+        if (err) throw err;
+        res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
+    });
+});
+
+//add new user
+app.post('/api/users', (req, res) => {
+    let data = {
+        firstname: req.body.firstname, lastname: req.body.lastname, email: req.body.email,
+        pubKey: req.body.pubKey
+    };
+    let sql = "INSERT INTO users SET ?";
+    let query = conn.query(sql, data, (err, results) => {
+        if (err) throw err;
+        res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
+    });
+});
+
+//update user
+app.put('/api/users/:id', (req, res) => {
+    let sql = "UPDATE users SET firstname='" + req.body.firstname + "', lastname='" + req.body.lastname
+        + "', email='" + req.body.email + "', pubKey='" + req.body.pubKey + "' WHERE user_id=" + req.params.user_id;
+    let query = conn.query(sql, (err, results) => {
+        if (err) throw err;
+        res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
+    });
+});
+
+//Delete user
+app.delete('/api/users/:id', (req, res) => {
+    let sql = "DELETE FROM users WHERE user_id=" + req.params.user_id + "";
+    let query = conn.query(sql, (err, results) => {
+        if (err) throw err;
+        res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
+    });
+});
+
+//Server listening
+app.listen(3000, () => {
+    console.log('Server started on port 3000...');
+});
