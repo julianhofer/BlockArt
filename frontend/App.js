320ed7c03d4de516958033fea3cb142827b1a813
Julian <jh187@hdm-stuttgart.de>
2020-01-16 09:38:54 +0100

    wip blockchain connection, edit ownership.slo, edit gitignore
    gitignore ignores dynamic builds in the frontend assets directory

--- frontend/App.js
+++ frontend/App.js
@@ -1,9 +1,12 @@
+
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
-import './ethers';
+// import { infuraProvider } from './ethers';
+
 
 
 export default class HelloWorldApp extends Component {
+
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
--- frontend/ethers.js
+++ frontend/ethers.js
@@ -1,6 +1,6 @@
-
 import { ethers } from 'ethers';
 import 'ethers/dist/shims.js';
+// import 'ethers/dist/shims.js';
 // const ethers = require('ethers');
 
 // Once a Ganache node is running, it behaves very similar to a
@@ -13,12 +13,32 @@ import 'ethers/dist/shims.js';
 // const signer0 = provider.getSigner(0);
 // const signer1 = provider.getSigner(1);
 
-let infuraProvider = new ethers.providers.InfuraProvider('https://ropsten.infura.io/v3/6a9086d09c8a4e0e99c279571ee00bad');
-// new ethers.providers.InfuraProvider([network = “homestead”][ , apiAccessToken]);
-console.debug("Infura: ", infuraProvider.getBlockNumber());
+const address = "0x5b3A61104f9Db9488880389F65b33eB0a1710D8B";
 
-const httpProvider = new ethers.providers.JsonRpcProvider('http://localhost:7545');
+const infuraProvider = new ethers.providers.InfuraProvider('https://ropsten.infura.io/v3/6a9086d09c8a4e0e99c279571ee00bad');
+// module.exports = { infuraProvider };
+
+
+infuraProvider.getBalance(address).then((balance) => {
+
+    // balance is a BigNumber (in wei); format is as a sting (in ether)
+    let etherString = ethers.utils.formatEther(balance);
+
+    console.debug("Balance: " + etherString);
+});
+console.log("Balance: " + etherString);
+console.log("log");
+console.debug("debug");
+
+infuraProvider.getNetwork().then((network) => {
+
+    console.log("Infura: ", network);
+});
+console.log("Infura: ", network);
+
+
+// const httpProvider = new ethers.providers.JsonRpcProvider('http://localhost:7545');
 // const getConnection = new ethers(httpProvider);
-console.debug("Account: ", httpProvider.getNetwork());
+// console.debug("Account: ", httpProvider.getNetwork());
 
 // module.exports = { getConnection };
