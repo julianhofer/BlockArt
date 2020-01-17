eecf7c7ecd24e9b13bed76b26af1aa7e10acbb87
Julian Hofer <j.hofer@binarystate.com>
2020-01-17 16:39:46 +0100

    add Blockchain Connection via Infura

---
+++ frontend/BCConnection.js
@@ -0,0 +1,8 @@
+import { ethers } from 'ethers';
+import 'ethers/dist/shims.js';
+
+
+// const url = "http://localhost:7545";
+// const provider = new ethers.providers.JsonRpcProvider(url);
+
+const infuraProvider = new ethers.providers.InfuraProvider('https://ropsten.infura.io/v3/6a9086d09c8a4e0e99c279571ee00bad');
--- frontend/ethers.js
+++ frontend/ethers.js
@@ -1,23 +1,13 @@
 import { ethers } from 'ethers';
 import 'ethers/dist/shims.js';
-// import 'ethers/dist/shims.js';
-// const ethers = require('ethers');
+const infuraProvider = require('./BCConnection');
 
-// Once a Ganache node is running, it behaves very similar to a
-// JSON-RPC API node.
-
-// const url = "http://localhost:7545";
-// const provider = new ethers.providers.JsonRpcProvider(url);
 
 // Getting the accounts
 // const signer0 = provider.getSigner(0);
 // const signer1 = provider.getSigner(1);
 
 const address = "0x5b3A61104f9Db9488880389F65b33eB0a1710D8B";
-
-const infuraProvider = new ethers.providers.InfuraProvider('https://ropsten.infura.io/v3/6a9086d09c8a4e0e99c279571ee00bad');
-// module.exports = { infuraProvider };
-
 
 infuraProvider.getBalance(address).then((balance) => {
 
@@ -35,10 +25,3 @@ infuraProvider.getNetwork().then((network) => {
     console.log("Infura: ", network);
 });
 console.log("Infura: ", network);
-
-
-// const httpProvider = new ethers.providers.JsonRpcProvider('http://localhost:7545');
-// const getConnection = new ethers(httpProvider);
-// console.debug("Account: ", httpProvider.getNetwork());
-
-// module.exports = { getConnection };
