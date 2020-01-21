d097b37979602abbbf87319584d5dc56755c3977
Julian <jh187@hdm-stuttgart.de>
2020-01-21 16:38:53 +0100

    fuck sakes - still no bc connection

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,148 @@
-
+import { ethers } from 'ethers';
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
-
-
+import smartcontract from './SmartContract/ArtWorkContract.json';
+// const owner = require('./ethers');
 
 export default class HelloWorldApp extends Component {
 
   render() {
+
+    let contractAbi = smartcontract.abi;
+    // console.log("contractAbi: ", contractAbi);
+
+    let provider = new ethers.providers.InfuraProvider('https://ropsten.infura.io/v3/6a9086d09c8a4e0e99c279571ee00bad');
+
+    let contractAddress = '0x36ed56f5e2160d46c3cbeee285298cbe2f0b0022';
+
+    let contract = new ethers.Contract(contractAddress, contractAbi, provider);
+
+    contract.currentOwner().then((currentOwner) => {
+
+      console.log("currentOwner: ", currentOwner)
+
+    });
+
+    // let abi = [
+    //   {
+    //     "constant": false,
+    //     "inputs": [
+    //       {
+    //         "name": "_newOwner",
+    //         "type": "address"
+    //       }
+    //     ],
+    //     "name": "transferOwnership",
+    //     "outputs": [],
+    //     "payable": false,
+    //     "stateMutability": "nonpayable",
+    //     "type": "function"
+    //   },
+    //   {
+    //     "inputs": [
+    //       {
+    //         "name": "_artHash",
+    //         "type": "bytes32"
+    //       }
+    //     ],
+    //     "payable": false,
+    //     "stateMutability": "nonpayable",
+    //     "type": "constructor"
+    //   },
+    //   {
+    //     "anonymous": false,
+    //     "inputs": [
+    //       {
+    //         "indexed": true,
+    //         "name": "previousOwner",
+    //         "type": "address"
+    //       },
+    //       {
+    //         "indexed": true,
+    //         "name": "newOwner",
+    //         "type": "address"
+    //       }
+    //     ],
+    //     "name": "OwnershipTransferred",
+    //     "type": "event"
+    //   },
+    //   {
+    //     "constant": true,
+    //     "inputs": [],
+    //     "name": "artHash",
+    //     "outputs": [
+    //       {
+    //         "name": "",
+    //         "type": "bytes32"
+    //       }
+    //     ],
+    //     "payable": false,
+    //     "stateMutability": "view",
+    //     "type": "function"
+    //   },
+    //   {
+    //     "constant": true,
+    //     "inputs": [],
+    //     "name": "getLogLength",
+    //     "outputs": [
+    //       {
+    //         "name": "",
+    //         "type": "uint256"
+    //       }
+    //     ],
+    //     "payable": false,
+    //     "stateMutability": "view",
+    //     "type": "function"
+    //   },
+    //   {
+    //     "constant": true,
+    //     "inputs": [
+    //       {
+    //         "name": "",
+    //         "type": "uint256"
+    //       }
+    //     ],
+    //     "name": "logs",
+    //     "outputs": [
+    //       {
+    //         "name": "currentOwner",
+    //         "type": "address"
+    //       },
+    //       {
+    //         "name": "ownershipStartTime",
+    //         "type": "uint256"
+    //       },
+    //       {
+    //         "name": "ownershipEndTime",
+    //         "type": "uint256"
+    //       }
+    //     ],
+    //     "payable": false,
+    //     "stateMutability": "view",
+    //     "type": "function"
+    //   },
+    //   {
+    //     "constant": true,
+    //     "inputs": [],
+    //     "name": "owner",
+    //     "outputs": [
+    //       {
+    //         "name": "",
+    //         "type": "address"
+    //       }
+    //     ],
+    //     "payable": false,
+    //     "stateMutability": "view",
+    //     "type": "function"
+    //   }
+    // ];
+
+
+
+
+
+
+
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text>Hello, world!</Text>
--- frontend/ethers.js
+++ frontend/ethers.js
@@ -1,28 +1,39 @@
-import { ethers } from 'ethers';
-import 'ethers/dist/shims.js';
-const infuraProvider = require('./BCConnection');
+// const infuraProvider = require('./BCConnection');
+// let contract = require('./BCConnection');
+// import { contract } from './BCConnection';
 
+
+
+
+
+
+// contract.on("OwnershipTransferred", (previousOwner, newOwner) => {
+//     // Called when anyone changes the value
+//     console.log(previousOwner);
+//     console.log(newOwner);
+
+// });
 
 // Getting the accounts
 // const signer0 = provider.getSigner(0);
 // const signer1 = provider.getSigner(1);
 
-const address = "0x5b3A61104f9Db9488880389F65b33eB0a1710D8B";
+// let address = "0x5b3A61104f9Db9488880389F65b33eB0a1710D8B";
 
-infuraProvider.getBalance(address).then((balance) => {
+// infuraProvider.getBalance(address).then((balance) => {
 
-    // balance is a BigNumber (in wei); format is as a sting (in ether)
-    let etherString = ethers.utils.formatEther(balance);
+//     // balance is a BigNumber (in wei); format is as a sting (in ether)
+//     let etherString = ethers.utils.formatEther(balance);
 
-    console.debug("Balance: " + etherString);
-});
+//     console.debug("Balance: " + etherString);
+// });
 
-console.log("Balance: " + etherString);
-console.log("log");
-console.debug("debug");
+// console.log("Balance: " + etherString);
+// console.log("log");
+// console.debug("debug");
 
-infuraProvider.getNetwork().then((network) => {
+// infuraProvider.getNetwork().then((network) => {
 
-    console.log("Infura: ", network);
-});
-console.log("Infura: ", network);
+//     console.log("Infura: ", network);
+// });
+// console.log("Infura: ", network);
