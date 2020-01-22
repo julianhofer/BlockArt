b82c2a639c522d23c070c17f8e775f6fc809ebe3
unknown <jh187@hdm-stuttgart.de>
2020-01-22 12:31:44 +0100

    wip bc connection with ethers

--- frontend/ethers.js
+++ frontend/ethers.js
@@ -2,7 +2,140 @@
 // let contract = require('./BCConnection');
 // import { contract } from './BCConnection';
 
+// let contractAbi = smartcontract.abi;
+// console.log("contractAbi: ", contractAbi);
 
+// contract.on("OwnershipTransferred", (previousOwner, newOwner) => {
+//     // Called when anyone changes the value
+//     console.log(previousOwner);
+//     console.log(newOwner);
+
+// });
+
+provider = new ethers.providers.InfuraProvider('https://ropsten.infura.io/v3/6a9086d09c8a4e0e99c279571ee00bad');
+
+const contractAddress = '0x36ed56f5e2160d46c3cbeee285298cbe2f0b0022';
+
+const contract = new ethers.Contract(contractAddress, abi, provider);
+
+contract.currentOwner().then((currentOwner) => {
+
+    console.log("currentOwner: ", currentOwner)
+
+});
+const abi = [
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_newOwner",
+                "type": "address"
+            }
+        ],
+        "name": "transferOwnership",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "name": "_artHash",
+                "type": "bytes32"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "name": "previousOwner",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "name": "newOwner",
+                "type": "address"
+            }
+        ],
+        "name": "OwnershipTransferred",
+        "type": "event"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "artHash",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bytes32"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "getLogLength",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "logs",
+        "outputs": [
+            {
+                "name": "currentOwner",
+                "type": "address"
+            },
+            {
+                "name": "ownershipStartTime",
+                "type": "uint256"
+            },
+            {
+                "name": "ownershipEndTime",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+            {
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    }
+];
 
 
 
