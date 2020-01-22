59edb32f208e30067929731c5a8180b3daa893f9
unknown <jh187@hdm-stuttgart.de>
2020-01-22 13:27:29 +0100

    working connection from the backend! nodejs for the win

--- backend/ethers.js
+++ backend/ethers.js
@@ -1,20 +1,9 @@
-// const infuraProvider = require('./BCConnection');
-// let contract = require('./BCConnection');
-// import { contract } from './BCConnection';
+const ethers = require('ethers');
 
-// let contractAbi = smartcontract.abi;
-// console.log("contractAbi: ", contractAbi);
-
-// contract.on("OwnershipTransferred", (previousOwner, newOwner) => {
-//     // Called when anyone changes the value
-//     console.log(previousOwner);
-//     console.log(newOwner);
-
-// });
-
-provider = new ethers.providers.JsonRpcProvider('https://ropsten.infura.io/v3/6a9086d09c8a4e0e99c279571ee00bad');
+let provider = new ethers.providers.JsonRpcProvider('https://ropsten.infura.io/v3/6a9086d09c8a4e0e99c279571ee00bad');
 
 const contractAddress = '0x36ed56f5e2160d46c3cbeee285298cbe2f0b0022';
+
 
 const abi = [
     {
@@ -158,36 +147,3 @@ contract.on("OwnershipTransferred", (previousOwner, newOwner) => {
 
 });
 
-
-
-
-// contract.on("OwnershipTransferred", (previousOwner, newOwner) => {
-//     // Called when anyone changes the value
-//     console.log(previousOwner);
-//     console.log(newOwner);
-
-// });
-
-// Getting the accounts
-// const signer0 = provider.getSigner(0);
-// const signer1 = provider.getSigner(1);
-
-// let address = "0x5b3A61104f9Db9488880389F65b33eB0a1710D8B";
-
-// infuraProvider.getBalance(address).then((balance) => {
-
-//     // balance is a BigNumber (in wei); format is as a sting (in ether)
-//     let etherString = ethers.utils.formatEther(balance);
-
-//     console.debug("Balance: " + etherString);
-// });
-
-// console.log("Balance: " + etherString);
-// console.log("log");
-// console.debug("debug");
-
-// infuraProvider.getNetwork().then((network) => {
-
-//     console.log("Infura: ", network);
-// });
-// console.log("Infura: ", network);
