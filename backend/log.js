b3e8bf47a532fc8fc31552aa5f16036f7078a21d
Julian <jh187@hdm-stuttgart.de>
2020-01-23 18:28:14 +0100

    created own backend repo

--- backend/log.js
+++
@@ -1,9 +0,0 @@
-const contract = require('./ethers');
-
-contract.artHash().then((artHash) => {
-    console.log("artHash: ", artHash)
-});
-
-contract.logs([]).then((log) => {
-    console.log("log: ", log)
-});
