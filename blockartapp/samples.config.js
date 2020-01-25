d19e3cb4c70ce244d0ec47a84860defeea9a7ac8
Jon Gehrung <jg138@hdm-stuttgart.de>
2020-01-25 01:18:27 +0100

    new configured app in blockart directory

---
+++ blockartapp/samples.config.js
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2019, Okta, Inc. and/or its affiliates. All rights reserved.
+ * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+ 
+export default {
+  oidc: {
+    clientId: '0oa11l7czJyLZqqHf4x6',
+    redirectUri: 'com.okta.dev-665917:/callback',
+    endSessionRedirectUri: 'com.okta.dev-665917:/callback',
+    discoveryUri: 'https://dev-665917.okta.com/oauth2/default',
+    scopes: ['openid', 'profile', 'offline_access'],
+    requireHardwareBackedKeyStore: false,
+  }
+};
